bb307f19d7962d3371efbe3775e83d8a95163aa3
Kaviya Dhanapal <kaviya.dhpl@gmail.com>
2022-09-25 00:03:46 +0200

    background and logo added

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 import Questionary from '../components/Questionary'
 
 //import '/src/assets/stylesheets/App.css'
-import Game from "/src/components/Game.jsx"
+// import Game from "/src/components/Game.jsx"
 
 export default function App() {
-
   return (
-    <div className="App">
-      {/* <Questionary /> */}
-      <Game />
+    <div className="App" >
+      <Questionary />
+      {/* <Game /> */}
     </div>
   )
 }
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,11 +1,5 @@
-
-
-
 export default function Question(props) {
-
-
   const questions = ["question1","question2","question3"];
-
   return (
     <div>
       <h2>{questions[props.index]}</h2>
