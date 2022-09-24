c6c3250b28487346caea55cc7c6ce3bc8ba95c7c
Sergio Pizarro <sergio.pizarroseg@gmail.com>
2022-09-24 16:27:04 +0200

    starting the questionary format

---
+++ src/questionary/Questionary.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './questionary.css'
+
+const Questionary = () => {
+  return (
+    <div className='container'>
+
+      <div className='question'>
+      </div>
+
+
+      <ul className='answers'>
+        <li className='answer'>Question 1</li>
+        <li className='answer'>Question 2</li>
+        <li className='answer'>Question 3</li>
+        <li className='answer'>Question 4</li>
+      </ul>
+
+      <button className="submit"></button>
+
+
+
+
+    </div>
+
+  )
+}
+
+export default Questionary
